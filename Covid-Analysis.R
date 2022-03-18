c511d51d8981d8816c8616eff30952eea0a60768
Bikramjeet Ladania <45115140+bsladania@users.noreply.github.com>
2022-03-17 21:39:50 -0400

    script

---
+++ Covid-Analysis.R
@@ -0,0 +1,53 @@
+rm(list=ls()) #remove all variables stored previously
+
+install.packages("Hmisc") #Download the package
+
+require("Hmisc") #Activate the package
+
+data = read.csv("F:/R/COVID-19 Analyzer/DataSet/COVID19_line_list_data.csv")
+
+View(data)
+describe(data) #Hmisc command
+
+#Cleaned up death column
+data$death_dummy <- as.integer(data$death != 0)
+
+#unique values
+unique(data$death_dummy)
+
+#death rate
+sum(data$death_dummy) / nrow(data)
+
+#Age
+
+#CLAIM:::::::::::: people who die are older
+
+dead = subset(data,death_dummy == 1)
+alive = subset(data,death_dummy == 0)
+
+mean(dead$age, na.rm = TRUE)
+mean(alive$age, na.rm = TRUE)
+
+#Is this statistically significant?
+t.test(alive$age, dead$age, alternative = "two.sided", conf.level = 0.95)
+
+#normally, if p-value < 0.05, we rehect null hypothesis
+#here, p-value ~ 0, so we reject the null hypothesis and conclude that this is 
+#statistically significant
+
+#CLAIM:::::::::::::: Gender has no effect
+
+men = subset(data, gender == "male")
+women = subset(data, gender == "female")
+
+mean(men$death_dummy, na.rm = TRUE) #8.5%
+mean(women$death_dummy, na.rm = TRUE) #3.7%
+
+#Is this statistically significant?
+t.test(men$death_dummy, women$death_dummy, alternative = "two.sided", 
+       conf.level = 0.95)
+
+#men have from 0.8% to 8.8% higher chance of dying.
+#p-value = 0.002 < 0.05, so this is statistically significant
+
+
